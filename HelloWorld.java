1e5b325f8ff0d8a934262cf78e85491337580842
hruthingali <hruthingd123@gmail.com>
2025-02-24 15:33:26 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         // Print Hello, World! to the console
-        System.out.println("Hello, World! welcome to jenkins , this is a jenkins dashboard,i hope you understand");
+        System.out.println("Hello, World! welcome to jenkins , this is a jenkins dashboard,i hope you understand, please  be careful");
     }
 }
